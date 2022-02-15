3cad042ff2a36342b51c522bc7b9b2c389c10520
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-02-15 15:19:31 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var app = express();
+var port = process.env.port||3000;
+app.get("/",function(req,res){
+       res.send("TODO API ROOT");
+});
+app.listen(port);
