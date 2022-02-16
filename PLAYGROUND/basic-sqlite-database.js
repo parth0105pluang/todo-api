40bc78f7af2a3c903f96f905eed20e3f2e44657b
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-02-16 17:23:39 +0530

    find

--- PLAYGROUND/basic-sqlite-database.js
+++ PLAYGROUND/basic-sqlite-database.js
@@ -17,6 +17,7 @@ var Todo = sequelize.define("todo",{
             defaultValue: false
     }
 })
+/*
 sequelize.sync().then(()=>{
      console.log("everything is synced");
      Todo.create({
@@ -27,3 +28,26 @@ sequelize.sync().then(()=>{
            console.log(todo);
      });
 });
+*/
+sequelize.sync({
+         force: true
+}).then(function(){console.log("every thing is synced");
+Todo.create({
+    description:"Take trash out"
+}).then(function(todo){
+       return Todo.create({
+             description: "clean office"
+       });
+}).then(function(){
+    return Todo.findById(1)
+}).then(function(todo){
+       if(todo){
+           console.log(todo.toJSON());
+       }else{
+           console.log("no todo found");
+       }
+})
+.catch(function(e){
+        console.log(e);
+});
+})
