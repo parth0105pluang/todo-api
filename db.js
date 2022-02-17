0cf0c0bb4dce7dff74b915066f485b5074ac5e60
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-02-17 09:40:34 +0530

    postgress

--- db.js
+++ db.js
@@ -3,9 +3,23 @@ var env = process.env.NODE_ENV || "development";
 var sequelize;
 if(env === 'production' ){
 
-    sequelize = new Sequelize(process.env.DATABASE_URL,{
+    /*sequelize = new Sequelize(process.env.DATABASE_URL,{
           "dialect": "postgres"
 
+    });*/
+    const sequelize = new Sequelize({
+        database: process.env.POSTGRES_DB,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+         }
     });
 }else{
      sequelize = new Sequelize(undefined,undefined,undefined,{
