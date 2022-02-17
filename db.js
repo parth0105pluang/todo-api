601a9b049828c8c571dce65b8f25495ad6f7e3e3
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-02-17 08:14:30 +0530

    postgress

--- db.js
+++ db.js
@@ -3,7 +3,10 @@ var env = process.env.NODE_ENV || "development";
 var sequelize;
 if(env === 'production' ){
     sequelize = new Sequelize(process.env.DATABASE_URL,{
-          dialect: "postgres"
+          dialect: "postgres",
+          "dialectOptions": {
+            "ssl": true
+          }
 
     });
 }else{
